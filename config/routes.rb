168fb321b4b0f7ab242cfd1e907e99796bc95d94
JakeCataford <JakeCataford@Gmail.com>
2013-07-23 13:02:02 -0400

    Updated to work with rails 4.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GoogleAuth::Engine.routes.draw do
-  match '/login', :to => 'sessions#new', :as => :login
-  match '/auth/g/callback', :to => 'sessions#create'
-  match '/auth/failure', :to => 'sessions#failure'
+  match '/login', :to => 'sessions#new', :as => :login, via: [:get, :post]
+  match '/auth/g/callback', :to => 'sessions#create', via: [:get, :post]
+  match '/auth/failure', :to => 'sessions#failure', via: [:get, :post]
 end
